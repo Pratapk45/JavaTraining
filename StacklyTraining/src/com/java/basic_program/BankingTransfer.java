dfa53b43efcf21686cce921e23c210644375afa0
Pratapk45 <pratapkachare45@gmail.com>
2026-07-08 22:36:52 +0530

    Adding package Looping statement

--- StacklyTraining/src/com/java/basic_program/BankingTransfer.java
+++ StacklyTraining/src/com/java/basic_program/BankingTransfer.java
@@ -4,34 +4,31 @@ import java.util.Scanner;
 
 public class BankingTransfer {
 
-		    static double balance = 10000;
+	static double balance = 10000;
 
-		    public static void deposit(double amount) {
-		        if (amount > 0) {
-		            balance += amount;
-		            
-		            System.out.println("Deposit Successful.");
-		            System.out.println("Available Balance :"+balance+" Rs");
-		        } else {
-		            System.out.println("Invalid Deposit Amount.");
-		        }
-		    }
+	public static void deposit(double amount) {
+		if (amount > 0) {
+			balance += amount;
 
-		    public static void withdraw(double amount) {
-		    	if(amount>0 && amount<=balance) {
-		    		balance-=amount;
-		    		System.out.println("Withdrawal Successfuly");
-		    		System.out.println("Remaining balance :"+balance +"Rs");
-		    	}
-		    	else if(amount<=0) {
-		    		System.out.println("Invalid Withdrawal Amount.");
-		    	}
-		    	else if( amount>balance){
-		    		System.out.println("Insufficient Balance.");
-		    	}
-		    	else{
-		    		System.out.println("Thank you");
-		    	}
+			System.out.println("Deposit Successful.");
+			System.out.println("Available Balance :" + balance + " Rs");
+		} else {
+			System.out.println("Invalid Deposit Amount.");
+		}
+	}
+
+	public static void withdraw(double amount) {
+		if (amount > 0 && amount <= balance) {
+			balance -= amount;
+			System.out.println("Withdrawal Successfuly");
+			System.out.println("Remaining balance :" + balance + "Rs");
+		} else if (amount <= 0) {
+			System.out.println("Invalid Withdrawal Amount.");
+		} else if (amount > balance) {
+			System.out.println("Insufficient Balance.");
+		} else {
+			System.out.println("Thank you");
+		}
 //		        if (amount <= 0) {
 //		            System.out.println("Invalid Withdrawal Amount.");
 //		        } else if (amount > balance) {
@@ -40,51 +37,51 @@ public class BankingTransfer {
 //		            balance -= amount;
 //		            System.out.println("Withdrawal Successful.");
 //		        }
-		    }
+	}
 
-		    public static void checkBalance() {
-		        System.out.println("Current Balance: ₹" + balance);
-		    }
+	public static void checkBalance() {
+		System.out.println("Current Balance: ₹" + balance);
+	}
 
-		    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		        Scanner sc = new Scanner(System.in);
-		        int choice;
+		Scanner sc = new Scanner(System.in);
+		int choice;
 
-		        do {
-		            System.out.println("\n===== BANK MENU =====");
-		            System.out.println("1. Deposit");
-		            System.out.println("2. Withdraw");
-		            System.out.println("3. Check Balance");
-		            System.out.println("4. Exit");
-		            System.out.print("Enter Choice: ");
-		            choice = sc.nextInt();
+		do {
+			System.out.println("\n===== BANK MENU =====");
+			System.out.println("1. Deposit");
+			System.out.println("2. Withdraw");
+			System.out.println("3. Check Balance");
+			System.out.println("4. Exit");
+			System.out.print("Enter Choice: ");
+			choice = sc.nextInt();
 
-		            switch (choice) {
-		                case 1:
-		                    System.out.print("Enter Deposit Amount: ");
-		                    deposit(sc.nextDouble());
-		                    break;
+			switch (choice) {
+			case 1:
+				System.out.print("Enter Deposit Amount: ");
+				deposit(sc.nextDouble());
+				break;
 
-		                case 2:
-		                    System.out.print("Enter Withdrawal Amount: ");
-		                    BankingTransfer.withdraw(sc.nextDouble());
-		                    break;
+			case 2:
+				System.out.print("Enter Withdrawal Amount: ");
+				BankingTransfer.withdraw(sc.nextDouble());
+				break;
 
-		                case 3:
-		                    checkBalance();
-		                    break;
+			case 3:
+				checkBalance();
+				break;
 
-		                case 4:
-		                    System.out.println("Thank You for Banking!");
-		                    break;
+			case 4:
+				System.out.println("Thank You for Banking!");
+				break;
 
-		                default:
-		                    System.out.println("Invalid Choice.");
-		            }
+			default:
+				System.out.println("Invalid Choice.");
+			}
 
-		        } while (choice != 4);
+		} while (choice != 4);
 
-		        sc.close();
-		    }
-		}
+		sc.close();
+	}
+}
---
+++ StacklyTraining/src/com/java/looping_statement/Pattern.java
@@ -0,0 +1,55 @@
+package com.java.looping_statement;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 7;
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("-----------------");
+
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("-------Pyramid---------");
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 1; k <= (2 * i - 1); k++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+		System.out.println("--------Inverted Pyramid-------");
+
+		for (int i = n; i >= 1; i--) {
+
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			for (int k = 1; k <= (2 * i - 1); k++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+
+}
